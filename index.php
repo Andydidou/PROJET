2f95cce301be669193d84f98a0078f9fa490499d
Covid <81747894+Andydidou@users.noreply.github.com>
2021-04-01 22:14:34 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Vaccination - Covid-19</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="landing is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header" class="alt">
+					<nav id="nav">
+						<ul>
+							<li><a href="index.php">Accueil</a></li>
+							<li><a href="centre.php">Les centres</a></li>
+
+							<!--afficher que si connecter-->
+							<li><a href="#" class="icon solid fa-angle-down">Espace personnel</a>
+								<ul>
+									<li><a href="information.php">Informations</a></li>
+									<li><a href="gestion.php">Gestion des rendez-vous</a></li>
+								</ul>
+							</li>
+							<!---->	
+						
+							
+							<!--enlever si connecter-->
+							<li><a href="#" class="button">Connexion</a></li>
+							<!---->
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Banner -->
+				<section id="banner">
+					<h2>Covid-19</h2>
+					<p>Prenez rapidement un rendez-vous dans un centre de vaccination près de chez vous !</p>
+					<ul class="actions special">
+						<li><a href="#" class="button primary">S'inscrire</a></li>
+					</ul>
+				</section>
+
+			<!-- Main -->
+				<section id="main" class="container">
+
+					<section class="box special">
+						<header class="major">
+							<h2>Bienvenue sur notre site</h2>
+							<p>Notre but est de vous permettre de prendre un rendez-vous rapidement <br/>
+							   pour vous faire vacciner contre le Covid-19.<br>
+							   Si vous souhaiter réservé un créneau, veuillez vous connecter.<br>
+						       Si vous n'avez pas de compte créer en un ici :<br>
+							<a>Créer un compte</a></p>
+							
+						</header>
+					</section>
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="copyright">
+						<li>&copy; Titou. Tout droit réservés.</li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
